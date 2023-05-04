725d9028837269a1a3172feed1ebc4af63df1039
Pavel Durov <p3ld3v@pm.me>
2023-05-04 14:10:03 +0100

    event

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,33 @@
 #include <iostream>
 using std::cout;
 
+#define LINUX
+#define X86_32
+
+#include "dr_api.h"
+
+static void event_exit(void);
+static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating);
+
 int main() {
     cout << "Hello World!" << std::endl;
     return 0;
+}
+
+DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
+{
+    /* register events */
+    dr_register_exit_event(event_exit);
+    dr_register_bb_event(event_basic_block);
+}
+
+static void event_exit(void)
+{
+    /* empty */
+}
+
+static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating)
+{
+    /* empty */
+    return DR_EMIT_DEFAULT;
 }
