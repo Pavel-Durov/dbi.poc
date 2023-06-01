c823e54e8f897d85e9d97881d2d5f56f8343ec89
Pavel Durov <p3ld3v@pm.me>
2023-06-01 13:50:08 +0100

    rename and param list

--- src/annotations/annotation.h
+++ src/annotations/annotation.h
@@ -1,29 +1,18 @@
-#ifndef _DYNAMORIO_ANNOTATIONS_H_
-#define _DYNAMORIO_ANNOTATIONS_H_ 1
- 
++#ifndef _MEMCLIENT_ANNOTATIONS_H_
++#define _MEMCLIENT_ANNOTATIONS_H_ 1
+
 #include "dr_annotations_asm.h"
- 
+
 /* To simplify project configuration, this pragma excludes the file from GCC warnings. */
 #ifdef __GNUC__
 # pragma GCC system_header
 #endif
- 
-#define DYNAMORIO_ANNOTATE_RUNNING_ON_DYNAMORIO() \
-    dynamorio_annotate_running_on_dynamorio()
- 
-#define DYNAMORIO_ANNOTATE_LOG(format, ...) \
-    DR_ANNOTATION(dynamorio_annotate_log, format, ##__VA_ARGS__)
- 
-#ifdef __cplusplus
+
+#define MEMCLIENT_MARK_MEM_DEFINED_IF_ADDRESSABLE(start, size) DR_ANNOTATION(memclient_mark_mem_defined_if_addressable, start, size)
+
+
 extern "C" {
-#endif
- 
-DR_DECLARE_ANNOTATION(char, dynamorio_annotate_running_on_dynamorio, (void));
- 
-DR_DECLARE_ANNOTATION(unsigned int, dynamorio_annotate_log, (const char *format, ...));
- 
-#ifdef __cplusplus
+    DR_DECLARE_ANNOTATION(void, memclient_mark_mem_defined_if_addressable, (void *p, size_t size));
 }
-#endif
- 
+
 #endif
