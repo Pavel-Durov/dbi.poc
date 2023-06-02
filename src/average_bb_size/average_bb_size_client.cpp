e8096dcd1b5e442555cec551a17229adbbd3fc9d
Pavel Durov <p3ld3v@pm.me>
2023-06-02 11:51:10 +0100

    thread local storage (tls) over locks

--- src/average_bb_size/average_bb_size_client.cpp
+++ src/average_bb_size/average_bb_size_client.cpp
@@ -1,6 +1,8 @@
 #include "dr_api.h"
-#define DISPLAY_STRING(msg) dr_printf("%s\n", msg)
+#include <stddef.h>
+#include <cstring>
 
+#define DISPLAY_STRING(msg) dr_printf("%s\n", msg)
 #define TESTALL(mask, var) (((mask) & (var)) == (mask))
 #define TESTANY(mask, var) (((mask) & (var)) != 0)
 
@@ -13,6 +15,7 @@ static bb_counts counts_as_built;
 void *as_built_lock;
 
 static bb_counts counts_dynamic;
+void *count_lock;
 
 /* Protected by the as_built_lock */
 static uint64 bbs_eflags_saved;
@@ -20,12 +23,16 @@ static uint64 bbs_eflags_saved;
 static void event_exit(void);
 static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating);
 
-DR_EXPORT void
-dr_client_main(client_id_t id, int argc, const char *argv[])
+static void event_thread_init(void *drcontext);
+static void event_thread_exit(void *drcontext);
+
+DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
 {
     /* register events */
     dr_register_exit_event(event_exit);
     dr_register_bb_event(event_basic_block);
+    dr_register_thread_init_event(event_thread_init);
+    dr_register_thread_exit_event(event_thread_exit);
    /* initialize lock */
    as_built_lock = dr_mutex_create();
 }
@@ -54,13 +61,35 @@ static void event_exit(void)
 
     /* free mutex */
     dr_mutex_destroy(as_built_lock);
+    dr_mutex_destroy(count_lock);
 }
+
+ static void event_thread_init(void *drcontext)
+ {
+     /* create an instance of our data structure for THIS thread */
+     bb_counts *counts = (bb_counts *)dr_thread_alloc(drcontext, sizeof(bb_counts));
+     /* store it in the slot provided in the drcontext */
+     dr_set_tls_field(drcontext, counts);
+     memset(counts, 0, sizeof(bb_counts));
+ }
+
+ static void event_thread_exit(void *drcontext)
+ {
+     bb_counts *counts = (bb_counts *) dr_get_tls_field(drcontext);
+     /* NOTE - if we so choose we could report per-thread sizes here. */
+     dr_mutex_lock(count_lock);
+     counts_dynamic.blocks += counts->blocks;
+     counts_dynamic.total_size += counts->total_size;
+     dr_mutex_unlock(count_lock);
+     dr_thread_free(drcontext, counts, sizeof(bb_counts));
+ }
 
 static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating)
 {
     uint num_instructions = 0;
     instr_t *instr, *where = NULL;
     bool eflags_saved = true;
+    bb_counts *counts = (bb_counts *) dr_get_tls_field(drcontext);
 
     /* count the number of instructions in this block */
     for (instr = instrlist_first(bb); instr != NULL; instr = instr_get_next(instr)) {
@@ -89,42 +118,46 @@ static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t
     if (eflags_saved) {
         where = instrlist_first(bb);
         // Looks like its causing 0 Segmentation fault (core dumped)
+        // Deprecated: This routine is equivalent to dr_save_reg() followed by dr_save_arith_flags_to_xax().
         dr_save_arith_flags(drcontext, bb, where, SPILL_SLOT_1);
     }
+    /* Spill a register to get a pointer to our TLS structure. */
+    dr_save_reg(drcontext, bb, where, DR_REG_XDI, SPILL_SLOT_2);
+    dr_insert_read_tls_field(drcontext, bb, where, DR_REG_XDI);
 
-     // Deprecated: This routine is equivalent to dr_save_reg() followed by dr_save_arith_flags_to_xax().
  #ifdef X86_32
      /* Since the counters are 64-bit we must use an add + an addc to increment.
       * The operations is still effectively atomic since we're only increasing
       * the count. */
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_add(drcontext,
+        INSTR_CREATE_add(drcontext,
                                OPND_CREATE_ABSMEM((byte *)&counts_dynamic.blocks, OPSZ_4),
-                               OPND_CREATE_INT8(1))));
+                               OPND_CREATE_INT8(1)));
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_adc(drcontext,
+        INSTR_CREATE_adc(drcontext,
                                OPND_CREATE_ABSMEM((byte *)&counts_dynamic.blocks + 4, OPSZ_4),
-                               OPND_CREATE_INT8(0))));
+                               OPND_CREATE_INT8(0)));
 
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_add(drcontext,
+         INSTR_CREATE_add(drcontext,
                                OPND_CREATE_ABSMEM((byte *)&counts_dynamic.total_size, OPSZ_4),
-                               OPND_CREATE_INT_32OR8(num_instructions))));
+                               OPND_CREATE_INT_32OR8(num_instructions)));
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_adc(drcontext,
+         INSTR_CREATE_adc(drcontext,
                                OPND_CREATE_ABSMEM((byte *)&counts_dynamic.total_size + 4, OPSZ_4),
-                               OPND_CREATE_INT8(0))));
+                               OPND_CREATE_INT8(0)));
  #else /* X86_64 */
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_inc(drcontext,
-                               OPND_CREATE_ABSMEM((byte *)&counts_dynamic.blocks, OPSZ_8))));
+        INSTR_CREATE_inc(drcontext, OPND_CREATE_ABSMEM((byte *)&counts_dynamic.blocks, OPSZ_8)));
      instrlist_meta_preinsert(bb, where,
-         LOCK(INSTR_CREATE_add(drcontext,
-                               OPND_CREATE_ABSMEM((byte *)&counts_dynamic.total_size, OPSZ_8),
-                               OPND_CREATE_INT_32OR8(num_instructions))));
+        INSTR_CREATE_add(drcontext,
+            OPND_CREATE_ABSMEM((byte *)&counts_dynamic.total_size, OPSZ_8),
+            OPND_CREATE_INT_32OR8(num_instructions)));
  #endif
-     if (eflags_saved) {
+    /* Restore spilled register. */
+    dr_restore_reg(drcontext, bb, where, DR_REG_XDI, SPILL_SLOT_2);
+    if (eflags_saved) {
         dr_restore_arith_flags(drcontext, bb, where, SPILL_SLOT_1);
-     }
+    }
     return DR_EMIT_DEFAULT;
 }
